53600fcd4789d92581af2ad5aee8c00996671cdf
mehtameet12 <mehtameet12@gmail.com>
2023-05-13 17:29:10 -0400

    typewriter effect added to the home page

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -80,9 +80,51 @@ linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
 
 /* GSAP Annimation */
 gsap.from('.home__img',{opacity: 0, duration: 1, delay:.25, x:60})
-gsap.from('.home__data',{opacity: 0, duration: 1, delay:.4, y:25})
-gsap.from('.home__greeting, .home__name, .home__profession',{opacity: 0, duration: 1, delay:.5, y:25, ease:'expo.out', stagger:.2})
+gsap.from('.home__data',{opacity: 0, duration: 1, delay:.3, y:25})
+gsap.from('.home__greeting, .home__name, .home__profession',{opacity: 0, duration: 1, delay:.25, y:25, ease:'expo.out', stagger:.2})
 
 gsap.from('.nav__logo, .nav__toggle',{opacity: 0, duration: 1, delay:.35, y:25, ease:'expo.out', stagger:.2})
 gsap.from('.nav__item',{opacity: 0, duration: 1, delay:.35, y:25, ease:'expo.out', stagger:.2})
 gsap.from('.home__social-icon',{opacity: 0, duration: 1, delay:.45, y:25, ease:'expo.out', stagger:.2})
+
+/* Typewriter Annimation */
+const typewriter = (txtElement, words, wait = 500) => {
+    let currentWordIndex = 0;
+    let currentTextIndex = 0;
+    let isDeleting = false;
+    const txt = txtElement.innerHTML;
+    const type = () => {
+      const currentWord = words[currentWordIndex];
+      const isEnd = currentTextIndex === currentWord.length;
+      if (isEnd && !isDeleting) {
+        setTimeout(() => {
+          isDeleting = true;
+        }, wait);
+      }
+      if (isDeleting) {
+        txtElement.innerHTML = currentWord.substring(0, currentTextIndex - 1);
+        currentTextIndex--;
+        if (currentTextIndex === 0) {
+          isDeleting = false;
+          currentWordIndex++;
+          if (currentWordIndex === words.length) {
+            currentWordIndex = 0;
+          }
+        }
+      } else {
+        txtElement.innerHTML = currentWord.substring(0, currentTextIndex + 1);
+        currentTextIndex++;
+      }
+      setTimeout(type, 100);
+    };
+    setTimeout(type, 100);
+  };
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const professionElement = document.querySelector("#home__profession");
+    const words = ["Web Developer", "Software Engineer", "Mechanical Designer", "Computer Science Student"];
+    setTimeout(() => {
+        typewriter(professionElement, words);
+      }, 1000);
+  });
+  
