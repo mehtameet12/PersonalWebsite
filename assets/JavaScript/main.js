acd9f3a685e7ddb7c550ac0e62e5a25cd927a16e
mehtameet12 <mehtameet12@gmail.com>
2023-06-02 21:19:32 -0400

    made changes for mobile view

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -44,7 +44,14 @@ window.addEventListener('scroll', scrollActive)
 /* Change Background Header */
 function scrollHeader(){
     const header = document.getElementById('header')
-    if(this.scrollY >= 750) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    const scrollThreshold = window.innerWidth < 576 ? 1705 : 750;
+    //if(this.scrollY >= 750) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+
+    if (this.scrollY >= scrollThreshold) {
+      header.classList.add('scroll-header');
+    } else {
+      header.classList.remove('scroll-header');
+  }
 }
 
 window.addEventListener('scroll', scrollHeader)
